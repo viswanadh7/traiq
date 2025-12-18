aa9625a7fb5eec4a051c0797396b6c56286d7292
viswanadh7 <mudunuruviswanadh@gmail.com>
2025-12-18 09:21:56 +0530

    feat(watchlist): add supabase function to get the latest stocks

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,28 @@
 import StockCard from "@/components/StockCard";
 import ThemedView from "@/components/ui/ThemedView";
-import { sampleData } from "@/constants/data";
-import React from "react";
+import supabase from "@/supabase";
+import { TStock } from "@/types/stock.type";
+import React, { useEffect, useState } from "react";
 import { FlatList } from "react-native";
 
 const WatchList = () => {
+  const [stocks, setStocks] = useState<TStock[]>();
+
+  useEffect(() => {
+    getStocks();
+  }, []);
+
+  const getStocks = async () => {
+    const { data, error } = await supabase.from("stocks").select("*");
+    if (error) {
+      console.log(error);
+    }
+    setStocks(data as TStock[]);
+  };
   return (
     <ThemedView>
       <FlatList
-        data={sampleData}
+        data={stocks}
         renderItem={({ item, index }) => <StockCard key={index} stock={item} />}
       />
     </ThemedView>
