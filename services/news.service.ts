1187d107e4eaf32b7a8289234cbe89fe693f4c60
viswanadh7 <mudunuruviswanadh@gmail.com>
2025-12-20 20:10:50 +0530

    fix(feed): improve news filtering and add removeOldNewsFromDB function

--- services/news.service.ts
+++ services/news.service.ts
@@ -14,8 +14,8 @@ const fetchNewsFromAPI = async () => {
     "economic growth",
   ];
   let promises = [];
-  for (let i = 0; i < 5; i++) {
-    promises.push(fetch(`${URL}&q=${encodeURIComponent(keywords[i])}`));
+  for (let keyword of keywords) {
+    promises.push(fetch(`${URL}&q=${encodeURIComponent(keyword)}`));
   }
   const res = await Promise.all(promises);
   const data = await Promise.all(res.map((item) => item.json()));
@@ -34,10 +34,15 @@ const uploadLatestNewsIntoDB = async (news: TNews[]) => {
   return await supabase.from("news").insert(news);
 };
 
+const removeOldNewsFromDB = async (old: Date) => {
+  return await supabase.from("news").delete().lt("created_at", old);
+};
+
 export const getLatestNews = async () => {
-  const today = new Date();
-  const start = new Date(today.setHours(0, 0, 0, 0));
-  const end = new Date(today.setHours(23, 59, 59, 999));
+  const start = new Date();
+  start.setHours(0, 0, 0, 0);
+  const end = new Date();
+  end.setHours(23, 59, 59, 999);
 
   // fetch latest news from the database
   const { data, error: fetchError } = await getLatestNewsFromDB(start, end);
@@ -49,14 +54,21 @@ export const getLatestNews = async () => {
   if (data?.length) {
     return data;
   }
+  // remove the old news from database
+  await removeOldNewsFromDB(start);
   // if no news fetched from database, fetch the latest news from api
   const fetchedNews: TNews[] = await fetchNewsFromAPI();
-  const filteredNews = fetchedNews.map((news) => ({
-    title: news.title,
-    description: news.description,
-    link: news.link,
-    image_url: news.image_url,
-  }));
+  let filteredNews = [];
+  for (let news of fetchedNews) {
+    if (news.title && news.description && news.image_url && news.link) {
+      filteredNews.push({
+        title: news.title,
+        description: news.description,
+        link: news.link,
+        image_url: news.image_url,
+      });
+    }
+  }
   // upload latest news fetched from api to the database
   const { error: uploadError } = await uploadLatestNewsIntoDB(filteredNews);
   if (!uploadError) {
