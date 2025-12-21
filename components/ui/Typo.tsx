35dec4f70de4b884f0467b8c4b3582434f401992
viswanadh7 <mudunuruviswanadh@gmail.com>
2025-12-21 11:32:18 +0530

    update Typo component to make it more reusable

--- components/ui/Typo.tsx
+++ components/ui/Typo.tsx
@@ -1,15 +1,19 @@
 import { useGlobalState } from "@/hooks/use-global-state";
 import React, { ReactNode } from "react";
-import { StyleProp, Text, TextStyle } from "react-native";
+import { StyleProp, Text, TextProps, TextStyle } from "react-native";
 
 type TypoProps = {
   children: ReactNode;
   styles?: StyleProp<TextStyle>;
-};
+} & TextProps;
 
-const Typo = ({ children, styles }: TypoProps) => {
+const Typo = ({ children, styles, ...rest }: TypoProps) => {
   const { theme } = useGlobalState();
-  return <Text style={[styles, { color: theme.text }]}>{children}</Text>;
+  return (
+    <Text style={[styles, { color: theme.text }]} {...rest}>
+      {children}
+    </Text>
+  );
 };
 
 export default Typo;
