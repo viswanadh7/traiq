a028abc6dbb9d894c9b6770d9789de4a5f09248a
viswanadh7 <mudunuruviswanadh@gmail.com>
2026-01-02 18:35:45 +0530

    add network connection status to provider

--- context/index.tsx
+++ context/index.tsx
@@ -1,17 +1,31 @@
 import { Colors, ColorsType } from "@/styles/theme";
-import { createContext, ReactNode } from "react";
+import NetInfo from "@react-native-community/netinfo";
+import { createContext, ReactNode, useEffect, useState } from "react";
 import { useColorScheme } from "react-native";
 
 type TContext = {
   theme: ColorsType;
+  isConnected: boolean | null;
 };
 
 export const Context = createContext<TContext>({} as TContext);
 
 const Provider = ({ children }: { children: ReactNode }) => {
   const theme = useColorScheme() === "dark" ? Colors.dark : Colors.light;
+  const [isConnected, setIsConnected] = useState<boolean | null>(null);
 
-  return <Context.Provider value={{ theme }}>{children}</Context.Provider>;
+  useEffect(() => {
+    const unsubscribe = NetInfo.addEventListener((state) =>
+      setIsConnected(state.isConnected)
+    );
+    return () => unsubscribe();
+  }, []);
+
+  return (
+    <Context.Provider value={{ theme, isConnected }}>
+      {children}
+    </Context.Provider>
+  );
 };
 
 export default Provider;
