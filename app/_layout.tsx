97795bc905c1246445392868ffe19f9f7cd3a2f9
viswanadh7 <mudunuruviswanadh@gmail.com>
2026-01-18 12:38:11 +0530

    feat(auth): integrate auth flow and splash screen in root layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,35 @@
+import CustomSplashScreen from "@/components/ui/CustomSplashScreen";
 import Provider from "@/context";
+import { useGlobalState } from "@/hooks/use-global-state";
 import { Stack } from "expo-router";
 import { StatusBar } from "react-native";
+import Toast from "react-native-toast-message";
 
-StatusBar.setBarStyle("default");
+function StackLayout() {
+  const { session, loading } = useGlobalState();
+  if (loading) {
+    return <CustomSplashScreen />;
+  }
+  return (
+    <Stack screenOptions={{ headerShown: false, animation: "none" }}>
+      {session ? (
+        <Stack.Screen name="(tabs)" />
+      ) : (
+        <>
+          <Stack.Screen name="signin/index" />
+          <Stack.Screen name="signup/index" />
+        </>
+      )}
+    </Stack>
+  );
+}
+
 export default function RootLayout() {
+  StatusBar.setBarStyle("default");
   return (
     <Provider>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      </Stack>
+      <StackLayout />
+      <Toast />
     </Provider>
   );
 }
