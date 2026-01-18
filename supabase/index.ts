f94c413f02ba23555639f2f1d0d6700e5321c897
viswanadh7 <mudunuruviswanadh@gmail.com>
2026-01-18 12:23:39 +0530

    feat(auth): add async-storage for session persistence

--- supabase/index.ts
+++ supabase/index.ts
@@ -1,7 +1,15 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL as string;
 const supabaseKey = process.env.EXPO_PUBLIC_SUPABASE_KEY as string;
-const supabase = createClient(supabaseUrl, supabaseKey);
+const supabase = createClient(supabaseUrl, supabaseKey, {
+  auth: {
+    storage: AsyncStorage,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
+});
 
 export default supabase;
