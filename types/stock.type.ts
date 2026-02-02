08c650bed51aff0c50c45e3c369dba97b8a414d8
viswanadh7 <mudunuruviswanadh@gmail.com>
2026-02-02 22:43:05 +0530

    feat(history): add THistory type definition

--- types/stock.type.ts
+++ types/stock.type.ts
@@ -14,3 +14,15 @@ export type TStock = {
   reason: string;
   created_at: Date;
 };
+
+export type THistory = {
+  id: number;
+  symbol: string;
+  price: number;
+  entry: number;
+  target: number;
+  stopLoss: number;
+  isTargetHit: boolean;
+  isStopLossHit: boolean;
+  isActive: boolean;
+};
