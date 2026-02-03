bfe621b795965684b39c3359643e2115b995ea31
viswanadh7 <mudunuruviswanadh@gmail.com>
2026-02-03 20:34:45 +0530

    feat(watchlist): redesign StockCard with calculations and theme styling

--- components/StockCard.tsx
+++ components/StockCard.tsx
@@ -1,7 +1,9 @@
 import { useGlobalState } from "@/hooks/use-global-state";
 import { TStock } from "@/types/stock.type";
+import { calculatePotential, calculateRR } from "@/utils/calculations";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import PatternChip from "./ui/PatternChip";
 import Typo from "./ui/Typo";
 
 type TStockCard = {
@@ -16,26 +18,84 @@ const StockCard = ({ stock }: TStockCard) => {
         styles.card,
         {
           backgroundColor: theme.card,
-          borderColor: theme.tabIconDefault,
         },
       ]}
     >
-      <View style={styles.header}>
-        <Typo styles={styles.symbol}>{stock.symbol}</Typo>
-        <Text
-          style={{
-            color: stock.close.changePercent > 0 ? "#16A34A" : "#DC2626",
-          }}
-        >
-          <Text style={styles.price}>₹ {stock.close.price}</Text> (
-          {stock.close.changePercent}%)
-        </Text>
+      <View>
+        <View style={styles.header}>
+          <Typo styles={styles.symbol}>{stock.symbol}</Typo>
+          <Typo styles={styles.price}>₹ {stock.close.price}</Typo>
+        </View>
+        <View style={styles.header}>
+          <PatternChip text={stock.pattern} />
+
+          <Text
+            style={{
+              color:
+                stock.close.changePercent > 0
+                  ? theme.target.color
+                  : theme.stopLoss.color,
+            }}
+          >
+            {stock.close.changePercent}%
+          </Text>
+        </View>
       </View>
-      <Typo>{stock.pattern}</Typo>
-      <View style={styles.priceContainer}>
-        <Typo styles={styles.prediction}>Entry: {stock.entry}</Typo>
-        <Typo styles={styles.prediction}>Target: {stock.target}</Typo>
-        <Typo styles={styles.prediction}>Stop loss: {stock.stopLoss}</Typo>
+      <View
+        style={[styles.priceContainer, { backgroundColor: theme.background }]}
+      >
+        <View>
+          <Typo styles={styles.setupLabel}>ENTRY</Typo>
+          <Typo styles={styles.setupPrice}>₹{stock.entry}</Typo>
+        </View>
+        <View
+          style={[
+            styles.verticalDivider,
+            { backgroundColor: theme.tabIconDefault },
+          ]}
+        />
+        <View>
+          <Text style={[styles.setupLabel, { color: theme.target.color }]}>
+            TARGET
+          </Text>
+          <Text style={[styles.setupPrice, { color: theme.target.color }]}>
+            ₹{stock.target}
+          </Text>
+        </View>
+        <View
+          style={[
+            styles.verticalDivider,
+            { backgroundColor: theme.tabIconDefault },
+          ]}
+        />
+        <View>
+          <Text style={[styles.setupLabel, { color: theme.stopLoss.color }]}>
+            STOP LOSS
+          </Text>
+          <Text style={[styles.setupPrice, { color: theme.stopLoss.color }]}>
+            ₹{stock.stopLoss}
+          </Text>
+        </View>
+      </View>
+      <View style={{ flexDirection: "row", gap: 20 }}>
+        <View style={{ flexDirection: "row", gap: 5, alignItems: "center" }}>
+          <Typo>Potential:</Typo>
+          <Text style={{ color: theme.target.color }}>
+            {calculatePotential(stock.entry, stock.target)}%
+          </Text>
+        </View>
+        <View
+          style={[
+            styles.verticalDivider,
+            { backgroundColor: theme.tabIconDefault },
+          ]}
+        />
+        <View style={{ flexDirection: "row", gap: 5, alignItems: "center" }}>
+          <Typo>R:R:</Typo>
+          <Text style={{ color: theme.rr }}>
+            1 : {calculateRR(stock.entry, stock.target, stock.stopLoss)}
+          </Text>
+        </View>
       </View>
       <Typo styles={styles.reason}>{stock.reason}</Typo>
     </View>
@@ -47,36 +107,52 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: "column",
     gap: 10,
-    padding: 10,
+    padding: 16,
     marginVertical: 8,
-    borderWidth: 0.5,
-    borderRadius: 6,
+    borderRadius: 10,
+    elevation: 4,
   },
   header: {
     flex: 1,
     flexDirection: "row",
     justifyContent: "space-between",
     alignItems: "center",
+    marginBottom: 5,
   },
   symbol: {
-    fontSize: 20,
-    fontWeight: "700",
+    fontSize: 18,
+    fontWeight: "bold",
+    letterSpacing: 0.8,
   },
   price: {
-    fontSize: 20,
-    fontWeight: "700",
+    fontSize: 18,
+    fontWeight: "bold",
   },
   priceContainer: {
     flex: 1,
     flexDirection: "row",
     justifyContent: "space-between",
+    paddingHorizontal: 20,
+    paddingVertical: 15,
+    borderRadius: 10,
   },
-  prediction: {
-    fontSize: 15,
+  setupLabel: {
+    fontSize: 12,
+    fontWeight: "thin",
+    textAlign: "center",
+  },
+  setupPrice: {
+    fontSize: 16,
+    fontWeight: "bold",
+    textAlign: "center",
   },
   reason: {
     fontSize: 13,
     fontWeight: "400",
+    opacity: 0.6,
+  },
+  verticalDivider: {
+    width: 0.5,
   },
 });
 
